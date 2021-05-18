ee7f8d6797e7e10848430a2ed3d036f2790d08b4
Ninh02 <ninhdo10021998@gmail.com>
2021-05-18 10:11:24 +0700

    bai tap

--- BT/Xay_dung_lop_stopWatch/index.php
+++ BT/Xay_dung_lop_stopWatch/index.php
@@ -1,1 +1,14 @@
 <?php
+include_once "stopWatch.php";
+$watch=new stopWatch();
+echo $watch->start();
+echo '<br>';
+$arr=[];
+for($i=0;$i<=100000;$i++){
+    $a=rand(0,100000);
+    $arr[]=$a;
+}
+rsort($arr);
+echo $watch->stop();
+echo '<br>';
+echo $watch->getElapsedTime();
--- BT/Xay_dung_lop_stopWatch/stopWatch.php
+++ BT/Xay_dung_lop_stopWatch/stopWatch.php
@@ -1,1 +1,21 @@
 <?php
+class stopWatch{
+    private float $startTime;
+    private float $stopTime;
+    public function __construct()
+    {
+        $this->startTime=microtime(true);
+    }
+    public function start()
+    {
+        return $this->startTime=microtime(true);
+    }
+    public function stop()
+    {
+        return $this->stopTime=microtime(true);
+    }
+    public function getElapsedTime()
+    {
+        return ($this->stopTime -$this->startTime)*1000;
+    }
+}
